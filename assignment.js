08d6e23fb275ddb8b992f79ad35d032ab2d0cff0
Nkaka Yves <nkakayves515@gmail.com>
2020-09-27 22:36:46 +0200

    editing javascript

--- assignment.js
+++ assignment.js
@@ -1,10 +1,24 @@
-function myfunc() {
-    var date=new date(document.getElementById("example-date-input"));
-    var dayin=date.getDay();
-    var month=date.getMonth();
-    var year=date.getYear();
-  alert(date);
-}
-function gen() {
-    var
+function weekdays() {
+    var day = document.getElementById("day").value;
+    var indate =day.split("-");
+    var d=parseInt(indate[2]);
+    var m=parseInt(indate[1]);
+    var y=parseInt(indate[0]);
+      var CC= (y-1)/100+1;
+      var birthday=(CC/4-2*CC-1+5*y/4+26*(m+1)/10+d)%7;
+      document.getElementById("display").innerHTML=Math.round(birthday);
+
+      var maleNa=["kwasi","kwando","kwabena","kwaku","yaw","kofi","kwame"];
+      var femaleNa=["akosua","adwoa","abena","akua","yaa","afua","ama"];
+      var gender=document.querySelector('input[name="optradio"]:checked').value;
+      console.log(gender);
+      if (gender=="male") {
+          console.log(maleNa[Math.round(birthday)]);
+          document.getElementById("names").innerHTML=maleNa[Math.round(birthday)];
+      } else if (gender=="female") {
+        console.log(femaleNa[Math.round(birthday)]);
+        document.getElementById("names").innerHTML=femaleNa[Math.round(birthday)];
+      } else {
+          console.log("Invalid");
+      }
 }
