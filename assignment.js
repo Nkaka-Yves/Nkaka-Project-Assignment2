0588578b0f038eaa0ecd8e55b2a2e70d35722230
Nkaka Yves <nkakayves515@gmail.com>
2020-09-27 14:27:01 +0200

    Add javascript content

--- assignment.js
+++ assignment.js
@@ -1,5 +1,11 @@
-var carName = "Volvo";
 function myFunction() {
-    var king= document.getElementById("birthdate").innerText;
-     document.getElementById("demo").innerHTML=king;
+   var inday=document.getElementById("day").value;
+   var inmonth=document.getElementById("month").value;
+    var inyear=document.getElementById("year").value;
+    var gender=document.getElementById("male").value;
+   var day=parseInt(inday);
+   var month=parseInt(inmonth);
+   var year=parseInt(inyear);
+   var sum=day+month+year;
+   document.getElementById("demo").innerHTML=sum;
   }
